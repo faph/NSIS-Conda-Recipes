63af398af784d2abbf13d3fde378d6c355e21426
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-19 17:21:15 +0100

    test success after each compile

--- ExecDos/run_test.bat
+++ ExecDos/run_test.bat
@@ -1,2 +1,1 @@
-for %%f in ("%PREFIX%\Examples\ExecDos\*.nsi") do "%PREFIX%\makensis" %%f
-if errorlevel 1 exit 1
+for %%f in ("%PREFIX%\Examples\ExecDos\*.nsi") do "%PREFIX%\makensis" %%f & if errorlevel 1 exit 1
