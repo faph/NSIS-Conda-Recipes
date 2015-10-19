7592a8fcd76e7f0c26f44500f561e5cf8d3b7596
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-19 18:48:23 +0100

    skip translate.nsi

--- Inetc/run_test.bat
+++ Inetc/run_test.bat
@@ -1,1 +1,8 @@
-for %%f in ("%PREFIX%\Examples\Inetc\*.nsi") do "%PREFIX%\makensis" %%f & if errorlevel 1 exit 1
+rem translate.nsi not valid
+
+for %%f in ("%PREFIX%\Examples\Inetc\*.nsi") do (
+	if not "%%~nxf" == "translate.nsi" (
+		"%PREFIX%\makensis" %%f
+		if errorlevel 1 exit 1
+	)
+)
