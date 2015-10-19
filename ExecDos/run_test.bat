4ba0d0c41b1c3c8710dbdcb0744b49550f322a2e
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-19 18:50:54 +0100

    readability linebreaks

--- ExecDos/run_test.bat
+++ ExecDos/run_test.bat
@@ -1,1 +1,4 @@
-for %%f in ("%PREFIX%\Examples\ExecDos\*.nsi") do "%PREFIX%\makensis" %%f & if errorlevel 1 exit 1
+for %%f in ("%PREFIX%\Examples\ExecDos\*.nsi") do (
+	"%PREFIX%\makensis" %%f
+	if errorlevel 1 exit 1
+)
