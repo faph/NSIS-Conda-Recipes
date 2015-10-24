2d860534e5f65fc54319bc51d2a57d7741e6e4b6
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-24 17:33:07 +0100

    deploy script

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+# Get all the packages in the build folder
+$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *.tar.bz2
+
+# Upload to anaconda.org
+binstar -t $env:binstar_token upload --no-progress $pkgs
