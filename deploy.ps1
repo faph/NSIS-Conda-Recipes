a5d95ff80e4e0530634c1ee854e9a8d4415d2857
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-24 19:21:06 +0100

    simplify conda install 5

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
-# Get all the packages in the build folder
-$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *.tar.bz2
+If (Test-Path $env:CONDA_BLD_PATH) {
+	# Get all the packages in the build folder
+	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *.tar.bz2
 
-# Upload to anaconda.org
-binstar -t "$env:binstar_token" upload --no-progress $pkgs
+	# Upload to anaconda.org
+	binstar -t "$env:binstar_token" upload --no-progress $pkgs
+}
