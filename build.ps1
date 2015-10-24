5b3d6cac9ff9cd5ae3f6d454779fc900ff0bc343
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-24 12:34:24 +0100

    test if package exist on anaconda.org

--- build.ps1
+++ build.ps1
@@ -1,4 +1,30 @@
+Function OnAnaconda {
+	param ([string]$pkg, [string]$channel)
+
+	$url = "$channel/win-64/$pkg"
+	$result = $true  # Assume on anaconda.org unless we get a 404 response.
+	try
+	{
+		$request = [System.Net.WebRequest]::Create($url)
+		$request.Method = 'HEAD'
+		$response = $request.GetResponse()
+		$response.Close()
+	}
+	catch [System.Net.WebException] {
+		$code = [int]$_.Exception.Response.StatusCode
+		$result = ($code -ne 404)
+	}
+	return $result
+}
+
+
 $pkgs = Get-ChildItem -exclude 'continuous-integration' | ?{ $_.PSIsContainer }
 foreach ($pkg in $pkgs) {
-   conda build $pkg --quiet
+	$pkg_name = (conda build $pkg --output | Get-ChildItem).Name
+	if (-not (OnAnaconda $pkg_name 'https://conda.anaconda.org/nsis')) {
+		Write-Host "Building package $pkg_name..."
+		conda build $pkg --quiet
+	} else {
+		Write-Host "Package $pkg_name already on anaconda.org. Skip building."
+	}
 }
