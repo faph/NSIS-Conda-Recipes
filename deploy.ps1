a8c8bea99ad728f4315f3091e3dbf34d9b63806b
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-24 19:52:43 +0100

    deploy messages

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,8 @@
 If (Test-Path "$env:CONDA_BLD_PATH\win-64") {
-	# Get all the packages in the build folder
 	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *.tar.bz2
 
-	# Upload to anaconda.org
+	"Uploading to anaconda.org..."
 	binstar -t "$env:binstar_token" upload --no-progress $pkgs
+} Else {
+	"Nothing to upload to anaconda.org"
 }
