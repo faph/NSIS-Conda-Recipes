414abbac6221ffb15357c0747f1022b3cad0e1b6
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-25 17:18:11 +0000

    revert to binstar upload

--- deploy.ps1
+++ deploy.ps1
@@ -2,8 +2,7 @@ If (Test-Path "$env:CONDA_BLD_PATH\win-64") {
 	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *-nsis_*.tar.bz2
 
 	"Uploading to anaconda.org..."
-	#binstar -t "$env:binstar_token" upload --no-progress $pkgs
-	anaconda upload --no-progress --user nsis -c https://conda.anaconda.org/t/$env:binstar_token/nsis $pkgs
+	binstar -t "$env:binstar_token" upload --no-progress $pkgs
 } Else {
 	"Nothing to upload to anaconda.org"
 }
