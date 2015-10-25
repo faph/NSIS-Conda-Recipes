4d31f8624be06b68216247525fef4982d28adc9f
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-25 15:58:53 +0000

    filter packages further

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 If (Test-Path "$env:CONDA_BLD_PATH\win-64") {
-	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *.tar.bz2
+	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *-nsis_*.tar.bz2
 
 	"Uploading to anaconda.org..."
 	binstar -t "$env:binstar_token" upload --no-progress $pkgs
