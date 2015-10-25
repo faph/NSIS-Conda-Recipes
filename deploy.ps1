e746549b0f343281b1c550390ab38c5ac4246aeb
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-10-25 17:26:00 +0000

    messages

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,11 @@ If (Test-Path "$env:CONDA_BLD_PATH\win-64") {
 
 	"Uploading to anaconda.org..."
 	binstar -t "$env:binstar_token" upload --no-progress $pkgs
+	If ($lastexitcode -eq 0) {
+		Add-AppveyorMessage -Message "Uploaded packages $pkgs" -Category Information
+	} Else {
+		Add-AppveyorMessage -Message "Failed uploading packages $pkgs" -Category Error
+	}
 } Else {
 	"Nothing to upload to anaconda.org"
 }
