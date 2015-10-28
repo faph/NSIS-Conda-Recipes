92d6b6cc3b9bb89e80011ee81134983b069918cd
Florenz Hollebrandse <FHollebrandse@Envirocentre.local>
2015-10-28 10:08:47 +0000

    use anaconda command

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@ If (Test-Path "$env:CONDA_BLD_PATH\win-64") {
 	$pkgs = Get-ChildItem -path $env:CONDA_BLD_PATH\win-64\* -include *-nsis_*.tar.bz2
 
 	"Uploading to anaconda.org..."
-	binstar -t "$env:binstar_token" upload --no-progress $pkgs
+	anaconda -t "$env:binstar_token" upload --no-progress $pkgs
 	If ($lastexitcode -eq 0) {
 		Add-AppveyorMessage -Message "Uploaded packages $pkgs" -Category Information
 	} Else {
